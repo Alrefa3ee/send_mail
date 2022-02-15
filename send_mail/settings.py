3f9722b04d3ba51c3f7641bd1dc66a15f219e7d4
Alrefa3ee <abd516693@icloud.com>
2022-02-15 09:44:36 +0200

    add req/Proc

--- send_mail/settings.py
+++ send_mail/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v#jq*_2j@_kgsnrbl(di=cnqq7&9az*1qcuu&3xb*p^t!i&x5f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,9 +123,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'api/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
