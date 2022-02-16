fec00a1b30e30bcd59b9eceb389963538f98d8b7
Alrefa3ee <abd516693@icloud.com>
2022-02-16 14:30:07 +0200

    edit

--- send_mail/settings.py
+++ send_mail/settings.py
@@ -53,8 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 CORS_ORIGIN_ALLOW_ALL = True  
 ROOT_URLCONF = 'send_mail.urls'
